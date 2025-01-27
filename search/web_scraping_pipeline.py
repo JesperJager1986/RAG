2a4d95d690c0a5302e0d3f45a57986f9958f4bf5
jesperthoftillemannjaeger <Jesperjag86@gmail.com>
2025-01-27 20:34:29 +0100

    get better responce

--- search/web_scraping_pipeline.py
+++ search/web_scraping_pipeline.py
@@ -111,7 +111,13 @@ class WebScrapingPipeline:
         print(f"Fetching {url}")
         """Fetch raw HTML content, falling back to Selenium if requests is unsuccessful."""
         try:
-            response = requests.get(url, timeout=10)
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+                #"Accept-Language": "en-US,en;q=0.9",
+                "Referer": "https://google.com"  # Some sites may check for a valid referer
+            }
+            response = requests.get(url, headers=headers, timeout=10)
             response.raise_for_status()
             self.raw_content = response.content
         except requests.exceptions.RequestException as e:
